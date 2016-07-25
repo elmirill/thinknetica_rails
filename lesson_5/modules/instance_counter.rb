e760dbbd3f6f7960c3764ba950cafad98f898aad
Tony Khaybulaev <ant.khay@gmail.com>
2016-07-25 15:43:01 +0700

    finish #5 optional task

--- lesson_5/modules/instance_counter.rb
+++ lesson_5/modules/instance_counter.rb
@@ -5,23 +5,14 @@ module InstanceCounter
   end
   
   module ClassMethods
-    @@instances = 0
-    
-    def instances
-      @@instances
-    end
-    
-#    protected
-    
-    def instances=(instances)
-      @@instances = instances
-    end
+    attr_accessor :instances
   end
   
   module InstanceMethods
     protected
     
     def register_instance
+      self.class.instances ||= 0
       self.class.instances += 1
     end
   end
